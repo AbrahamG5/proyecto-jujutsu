eb73c739ea3e97738b94077b30005ef675b12d16
Red <abrahamgg055@gmail.com>
2025-10-27 11:41:24 -0600

    avance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div className="min-h-screen min-w-screen bg-linear-gradient from-slate-950 via-slate-700 to-slate-400">
-      <h1 className="text-red-300">Coso</h1>
+    <div className="min-h-screen bg-linear-to-b from-slate-900 to-purple-400">
+      <div className="content-center justify-center items-center max-w-dvw">
+        <h1 className="text-white text-2xl font-semibold bg-slate-400 max-w-xs border-white border-2 rounded-2xl text-center m-auto ">
+          JujuPedia
+        </h1>
+      </div>
     </div>
   );
 }
