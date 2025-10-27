fc83144575b6c681f2f4eb81ae04a1d89804daa6
Red <abrahamgg055@gmail.com>
2025-10-27 12:42:15 -0600

    avance

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import Navbar from "./mainpage/components/navbar";
 
 function App() {
   return (
-    <div className="min-h-screen bg-linear-to-b from-slate-900 to-purple-400">
+    <div className="min-h-screen bg-slate-900 ">
       <Navbar />;
       <div className="content-center justify-center items-center max-w-dvw">
         <h1 className="text-white text-2xl font-semibold bg-slate-400 max-w-xs border-white border-2 rounded-2xl text-center m-auto mt-3">
           JujuPedia
         </h1>
       </div>
+      <div className="size-50 bg-linear-to-br from-blue-900 to-blue-300 rounded-2xl hover:scale-105 hover:transition-all hover:duration-300 hover:shadow-2xl hover:shadow-white/40 m-auto"></div>
     </div>
   );
 }
--- src/mainpage/components/Navbar.tsx
+++ src/mainpage/components/Navbar.tsx
@@ -2,13 +2,13 @@ function Navbar() {
   return (
     <div className="p-2">
       <nav
-        className="h-10 p-1 bg-linear-to-b from-indigo-800 to-indigo-500 shadow-lg
+        className=" flex h-10 p-1 bg-linear-to-b from-indigo-800 to-indigo-500 shadow-white shadow-xs
       border-b border-b-white items-center content-center rounded-2xl"
       >
         <input
           type="text"
           placeholder="Ingresa el personaje a buscar"
-          className="text-white bg-slate-500 mx-2 rounded-2xl pl-2 w-55"
+          className="text-white bg-slate-500 mx-2 rounded-2xl pl-2 w-57"
         />
         <button className="bg-slate-600 text-white w-16 rounded-2xl hover:bg-slate-500 transition-all duration-300 cursor-pointer border">
           Buscar
