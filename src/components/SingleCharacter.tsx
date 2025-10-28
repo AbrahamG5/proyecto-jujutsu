5d7d0423d84317ffed8cd4942b31458f9b0d2d11
AbrahamG5 <abrahamgg055@gmail.com>
2025-10-28 12:46:27 -0600

    avance

--- src/components/SingleCharacter.tsx
+++ src/components/SingleCharacter.tsx
@@ -7,15 +7,18 @@ const SingleCharacter = () => {
   const characterImageURL = getCharacter?.image_url;
 
   return (
-    <div className="flex flex-col w-80 h-96 bg-gradient-to-br from-blue-900 to-blue-300 rounded-2xl hover:scale-105 hover:transition-all hover:duration-300 hover:shadow-2xl hover:shadow-white/40 m-auto mt-4 hover:mt-8 items-center justify">
-      <h1 className="text-white text-xl p-2 bg-">
-        {getCharacter?.name || "Unknown"}
-      </h1>
+    <div className="flex flex-col w-80 h-96 bg-linear-to-br from-blue-900 to-blue-300 rounded-2xl hover:scale-105 hover:transition-all hover:duration-300 hover:shadow-2xl hover:shadow-white/40 m-auto mt-4 hover:mt-8 items-center justify p-2">
       <img
         src={`https://images.weserv.nl/?url=${characterImageURL}`}
         alt={`${getCharacter}`}
-        className="w-auto h-60"
+        className="w-auto h-60 mb-2"
       />
+      <hr className="bg-blue-400 h-1 w-full my-1"/>
+      <h1 className="text-white text-2xl mb-1 h-auto">
+        {getCharacter?.name || "Unknown"}
+      </h1>
+      <p className="text-white"><b>Edad: </b>{getCharacter?.age[0]} años</p>
+      <p className="text-white"><b>Categoría: </b> {getCharacter?.grade || "Sin categoría"}</p>
     </div>
   );
 };
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
-    <div className="content-center justify-center items-center max-w-dvw mt-2">
-      <h1 className="text-white text-2xl font-semibold bg-slate-400 max-w-xs border-white border-2 rounded-2xl text-center m-auto">
+    <div className=" justify-center items-center max-w-dvw mt-2">
+      <h1 className="text-white text-2xl font-semibold bg-slate-400 max-w-2xl border-white border-2 rounded-2xl text-center m-auto">
         JujuPedia
       </h1>
     </div>
